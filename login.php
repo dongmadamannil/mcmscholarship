95674172a315f33fd09be40c6ecaedc3e0c54b8f
afeefkp <93336792+afeefkp@users.noreply.github.com>
2022-10-11 14:28:13 +0530

    Update login.php
    change in design
    added navbar

--- login.php
+++ login.php
@@ -3,92 +3,95 @@
 <html lang="en">
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-body {font-family: Arial, Helvetica, sans-serif;}
-form {border: 3px solid #f1f1f1;}
-
-input[type=text], input[type=password] {
-  width: 100%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  box-sizing: border-box;
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+ <style>
+.vh-100 {
+    height: 100%!important;
 }
+.gradient-custom {
+/* fallback for old browsers */
+background: #6a11cb;
 
-.sub {
-  background-color: #5186DE;
-  color: white;
-  padding: 14px 20px;
-  margin: 8px 0;
-  border: none;
-  cursor: pointer;
-  width: 100%;
-}
+/* Chrome 10-25, Safari 5.1-6 */
+background: -webkit-linear-gradient(to right, rgba(106, 17, 203, 1), rgba(37, 117, 252, 1));
 
-.sub:hover {
-  opacity: 0.8;
-}
-
-.cancelbtn {
-  width: auto;
-  padding: 10px 18px;
-  background-color: #f44336;
-}
-
-.imgcontainer {
-  text-align: center;
-  margin: 24px 0 12px 0;
-}
-
-img.avatar {
-  width: 40%;
-  border-radius: 50%;
-}
-
-.container {
-  padding: 16px;
-}
-
-span.psw {
-  float: right;
-  padding-top: 16px;
-}
-.lgn{
-    height: 36%;
-    width: 40%;
-    border-radius: 15px;
-    position: absolute;
-    top:25%;
-left: 40%;
-}
-
-@media screen and (max-width: 300px) {
-  span.psw {
-     display: block;
-     float: none;
-  }
-  .cancelbtn {
-     width: 100%;
-  }
+/* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+background: linear-gradient(to right, rgba(106, 17, 203, 1), rgba(37, 117, 252, 1))
 }
 </style>
+
 </head>
 <body>
 
-<form action="" method="post" class="lgn">
-    <div class="container">
-    <label for="uname"><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="uname" required>
+<!-- navbar -->
 
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
+    <div class="navbar navbar-expand-lg bg-dark navbar-dark py-3">
+        <div class="container">
+            <a href="#" class="navbar-brand">MCM SCHOLARSHIP</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+           
+            <div class="collapse navbar-collapse" id="navmenu">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a href="mcmhome.html" class="nav-link">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://www.tkmce.ac.in/about-college.php" class="nav-link">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://www.tkmce.ac.in" class="nav-link">Official Website</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://www.tkmce.ac.in/contact.php" class="nav-link">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            
+        </div>
         
-    <input type="submit" value="Login" name="lgn" class="sub">
-      </div>
+    </div>
 
-  
-</form>
+
+<section class="vh-100 gradient-custom">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div class="card bg-dark text-white" style="border-radius: 1rem;">
+          <div class="card-body p-5 text-center">
+
+            <div class="mb-md-3 mt-md-2 pb-2">
+                <form action="" method="post" class="lgn">
+            
+                    <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+
+                    <div class="form-outline form-white mb-4">
+                    <input type="email" id="typeEmailX" class="form-control form-control-lg" placeholder="Email" name="uname" required/>
+
+                    </div>
+
+                    <div class="form-outline form-white mb-4">
+                    <input type="password" id="typePasswordX" class="form-control form-control-lg"placeholder="Password" name="psw" required />
+                    </div>
+
+                    <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
+
+                    <button class="btn btn-outline-light btn-lg px-5" type="submit" name="lgn">Login</button>
+
+                    </div>
+            
+                </form>
+
+              
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
 <?php
 $conn=mysqli_connect("localhost","root","","scholarship");
 if(isset($_POST['lgn']))
@@ -126,5 +129,7 @@ echo "<script>window.location='advisor_dashboard.html';</script>";
 }
 
 ?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
